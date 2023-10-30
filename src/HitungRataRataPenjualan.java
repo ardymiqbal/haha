a10a360496357e79211c67dad63e7939d3496567
Ardy <ardymiqbal@gmail.com>
2023-10-30 11:48:12 +0800

    tambahin fungsi

---
+++ src/HitungRataRataPenjualan.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HitungRataRataPenjualan {
+    /**
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        int jumlahPenjualan = scanner.nextInt();
+
+        int totalPenjualan = 0;
+        for (int i = 1; i <= jumlahPenjualan; i++) {
+            System.out.print("Masukkan Penghasilan penjualan barang pada hari ke-" + i + ": ");
+            int penjualan = scanner.nextInt();
+            totalPenjualan += penjualan;
+        }
+
+        double rataRata = (double) totalPenjualan / jumlahPenjualan;
+        System.out.println("Rata-rata Penghasilan penjualan barang adalah: " + rataRata);
+
+        scanner.close();
+    }
+}
